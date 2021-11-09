fad3d6f0a5a252ae7f55365823f949cc9ef7688b
CIPop <Cristian.Pop@microsoft.com>
2021-11-09 15:36:51 -0800

    Adding CA root download scripts.

---
+++ ca_root/download_ca_root.sh
@@ -0,0 +1,5 @@
+wget https://cacerts.digicert.com/BaltimoreCyberTrustRoot.crt.pem -O rsa_baltimore_ca.pem
+wget https://cacerts.digicert.com/DigiCertGlobalRootG2.crt.pem -O rsa_digicert_g2.pem
+wget https://cacerts.digicert.com/DigiCertGlobalRootG3.crt.pem -O ecc_digicert_g3.pem
+
+
--- openssl/show.sh
+++ openssl/show.sh
@@ -3,3 +3,4 @@
 NAME=${1?"Pass the certificate filename as the first parameter."}
 
 openssl x509 -in $NAME -noout -text -fingerprint
+
