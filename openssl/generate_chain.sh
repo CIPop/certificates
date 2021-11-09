2226beae54a8ceeed08be20f419050b4ff45729b
CIPop <Cristian.Pop@microsoft.com>
2021-11-09 23:06:33 +0000

    Cert gen fixes.

--- openssl/generate_chain.sh
+++ openssl/generate_chain.sh
@@ -14,12 +14,12 @@ openssl req -new -days 3650 -nodes -x509 -key ca_key.pem -out ca_cert.pem -subj 
 openssl ecparam -out i1_key.pem -name prime256v1 -genkey
 #openssl genrsa -out i1_key.pem 4096
 
-openssl req -new -key i1_key.pem -out i1_ec.csr -subj "/CN=$ICA1_NAME"
-openssl x509 -req -in i1_ec.csr -CA ca_cert.pem -CAkey ca_key.pem -CAcreateserial -out i1_cert.pem -days 3650 -sha256
+openssl req -new -key i1_key.pem -out i1.csr -subj "/CN=$ICA1_NAME"
+openssl x509 -req -in i1.csr -CA ca_cert.pem -CAkey ca_key.pem -CAcreateserial -out i1_cert.pem -days 3650 -sha256
 
 # Generate ICA2
 openssl ecparam -out i2_key.pem -name prime256v1 -genkey
 #openssl genrsa -out i2_key.pem 4096
 
-openssl req -new -key i2_key.pem -out i2_ec.csr -subj "/CN=$ICA2_NAME"
-openssl x509 -req -in i2_ec.csr -CA i1_cert.pem -CAkey i1_key.pem -CAcreateserial -out i2_cert.pem -days 3650 -sha256
+openssl req -new -key i2_key.pem -out i2.csr -subj "/CN=$ICA2_NAME"
+openssl x509 -req -in i2.csr -CA i1_cert.pem -CAkey i1_key.pem -CAcreateserial -out i2_cert.pem -days 3650 -sha256
